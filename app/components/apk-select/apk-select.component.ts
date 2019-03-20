6a26eaa19cf4195b9c17cd1747cc1a11be0565a7
koushikilaveni <kilaveni2477@ucumberlands.edu>
2019-03-20 15:06:55 -0400

    5 files modified

--- app/components/apk-select/apk-select.component.ts
+++ app/components/apk-select/apk-select.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'apk-select',
   templateUrl: './apk-select.component.html',
-  styleUrls: ['./apk-select.component.scss']
+  styleUrls: ['./apk-select.component.scss'],
+   encapsulation: ViewEncapsulation.None
 })
 export class ApkSelectComponent implements OnInit {
 
